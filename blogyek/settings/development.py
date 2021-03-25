7c038c5f7fa7896d9ccf46ed9f816de7d31e46a5
emre <tataruq38@gmail.com>
2021-03-25 22:09:06 +0300

    Upload Files

--- blogyek/settings/development.py
+++
@@ -1,6 +0,0 @@
-from blogyek.settings.base import *
-
-DEBUG = True
-
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
--- blogyek/settings/production.py
+++
@@ -1,5 +0,0 @@
-from blogyek.settings.base import *
-
-DEBUG = False
-
-STATIC_ROOT = BASE_DIR / 'static'
